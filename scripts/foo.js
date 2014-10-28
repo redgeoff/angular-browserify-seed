69b4ee74687d7af502f021124631e98d4e2cac5c
Geoff Cox <redgeoff@gmail.com>
2014-10-28 03:53:09 -0700

    refactor(foo): correct indentation

--- scripts/foo.js
+++ scripts/foo.js
@@ -5,11 +5,11 @@ var Bar = require('./bar'), bar = new Bar();
 module.exports = function ($q) {
 
   this.get = function () {
-	var defer = $q.defer();
-	setTimeout(function () {
-		defer.resolve('foo' + bar.get());
-	}, 100);
-	return defer.promise;
+  var defer = $q.defer();
+  setTimeout(function () {
+    defer.resolve('foo' + bar.get());
+  }, 100);
+  return defer.promise;
   };
 
 };
